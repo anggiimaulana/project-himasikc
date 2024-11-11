fb6b5be4818ae537c7a7aabe26e5409a4360cd43
FAJRIAG <FAZRYGGWP@GMAIL.COM>
2024-11-11 13:13:19 +0700

    <home 1.2>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HIMA-SIKC 2023/2024</title>
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/Home.css') }}">
 </head>
 <body>
     <header class="header">
