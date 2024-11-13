dc4485b892a838e5d406f9f7ae366f0736485b41
anggii <anggabos120@gmail.com>
2024-11-13 09:46:16 +0700

    program_v1

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,5 +1,5 @@
 <header class="bg-white">
-    <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-14 border-b border-gray-200"
+    <nav class="container mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-14 border-b border-gray-200"
         aria-label="Global">
         <div class="flex lg:flex-1">
             <a href="/" class="-m-1.5 p-1.5">
